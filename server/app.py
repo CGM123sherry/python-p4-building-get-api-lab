2d4c207cf1b6fd35a54e67d19f94a338e3321f5a
CGM123sherry <g.christinemurithi@gmail.com>
2024-10-06 18:28:53 +0300

    worked on the get api lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,31 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    # fetch all backery objects
+    bakeries=  Bakery.query.all()
+    # Convert to JSON format
+    return jsonify([bakery.to_dict() for bakery in bakeries])
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    # Fetch bakery by id or return 404 if not found
+    bakery = Bakery.query.get_or_404(id) 
+    # Convert to JSON
+    return jsonify(bakery.to_dict())  
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    # Sort by price (descending)
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    # Convert to JSON format
+    return jsonify([baked_good.to_dict() for baked_good in baked_goods])
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()  
+# Convert to JSON format
+    return jsonify(most_expensive.to_dict())  
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
